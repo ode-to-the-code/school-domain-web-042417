964213455bff7a5388cba01f13d9a7f309980e4c
ode-to-the-code <justcallmethis@gmail.com>
2017-05-16 19:10:35 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
 # code here!
+require 'pry'
+
+class School
+
+attr_accessor :name, :roster, :studentname, :grade
+
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+#
+  def add_student(studentname, grade)
+# says if its nil, set to this empty array, but if it's not nil, do nothing and let it retain its prior value
+    @roster[grade] ||= []
+    @roster[grade] << studentname
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    # @roster.map do |key, value|
+       @roster.each_value { |v| v.sort! }
+
+
+end
+end
